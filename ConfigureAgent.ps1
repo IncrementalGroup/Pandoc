415ca172a86a5fd5cf22568f6d0317e9525069ec
Zoe Mackay <41298038+zoemac11@users.noreply.github.com>
2019-02-04 20:23:40 +0000

    update config for vsts agent

--- ConfigureAgent.ps1
+++ ConfigureAgent.ps1
@@ -1,3 +1,23 @@
+<<<<<<< HEAD
+$url = https://incrementalgroup.visualstudio.com
+$token = Mfm7c7yd4chctx3u264icdb534dhgryzvsywgb2kvhmdya3oa6fq
+$pool = DockerTests
+$agentName = "ContainerTest"
+
+Write-Verbose -verbose "Configuring agent $AgentName for pool $pool"
+
+.\config.cmd --unattended `
+    --url $url `
+    --auth pat `
+    --token $token `
+    --pool $pool `
+    --agent $agentName `
+    --acceptteeula `
+    --replace `
+    --gituseschannel
+
+.\run.cmd
+=======
 $url = https://incrementalgroup.visualstudio.com/
 $token = Mfm7c7yd4chctx3u264icdb534dhgryzvsywgb2kvhmdya3oa6fq
 $pool = default
@@ -16,3 +36,4 @@ Write-Verbose -verbose "Configuring agent $AgentName for pool $pool"
     --gituseschannel
 
 .\run.cmd
+>>>>>>> 9e08e52d3405cdc42366d7b0b2439e622d8ed395
--- InstallAgent.ps1
+++ InstallAgent.ps1
@@ -1,3 +1,8 @@
+<<<<<<< HEAD
 Write-Verbose "Extracting Build Agent"
 Add-Type -AssemblyName System.IO.Compression.FileSystem;
+=======
+Write-Verbose "Extracting Build Agent"
+Add-Type -AssemblyName System.IO.Compression.FileSystem;
+>>>>>>> 9e08e52d3405cdc42366d7b0b2439e622d8ed395
 [System.IO.Compression.ZipFile]::ExtractToDirectory("vsts-agent-win-x64-2.144.0.zip", "$PWD")
